1dca641e7b58d0a8ad412a42686cf14aa46084e4
Hamzasehouli <sehoulihamza@yahoo.com>
2022-02-24 19:46:33 +0100

    Added UserPlicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Models\Cake;
 use App\Policies\CakePolicy;
+use App\Models\User;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -17,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         Cake::class => CakePolicy::class,
+        User::class => UserPolicy::class,
     ];
 
     /**
