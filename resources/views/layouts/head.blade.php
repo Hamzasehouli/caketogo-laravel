066dfc7b0d2fea98c57a4a3d5b13bd4ba46ce036
Hamzasehouli <sehoulihamza@yahoo.com>
2022-02-28 21:08:33 +0100

    Added header and footer components, finished footer

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,51 +8,14 @@
     <title>Document</title>
 </head>
 <body>
-    <nav class="flex top-0 left-0 w-full shadow-lg fixed bg-pink-500 text-white px-14 py-4 justify-between">
-      <ul class="flex justify-between">
-          <li class="pr-3 last:pr-0">
-              <a href="{{route('home')}}">Home</a>
-          </li>
-          <li class="pr-3 last:pr-0">
-              <a href="{{route('cakes')}}">Cakes</a>
-          </li>
-      </ul>
-      <ul class="flex justify-between">
-          @if (Auth::check())
-          <li class="pr-3 last:pr-0">
-              <p>Hello {{Auth::user()->name}}</p>
-          </li>
-          <li class="pr-3 last:pr-0">
-              <a href="{{route('profile')}}">Profile</a>
-          </li>
-          <li class="pr-3 last:pr-0">
-              <a href="#">Cart</a>
-          </li>
-          <li class="pr-3 last:pr-0">
-              <form method="post" action="{{route('logout')}}">
-                @csrf
-                @method('DELETE')
-                <button type="submit" href="">Logout</button>    
-              </form>
-          </li>
-              
-          @else
-              
-          <li class="pr-3 last:pr-0">
-              <a href="{{route('register')}}">Register</a>   
-          </li>
-          <li class="pr-3 last:pr-0">
-              <a href="{{route('login')}}">Login</a>  
-          </li>
-          @endif
-      </ul>
-    </nav>
+
+    <x-header></x-header>
+
     <main class="min-h-[100vh] mx-auto max-w-[1420px]">
         @yield('content')
     </main>
-    <footer class="bg-pink-500 w-full h-screen">
-<p>footer</p>
-    </footer>
 
+    <x-footer></x-footer> 
+    
 </body>
 </html>
