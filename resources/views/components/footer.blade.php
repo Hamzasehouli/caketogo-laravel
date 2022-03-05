6917f71a77f7f1ea91043475830d449dc3e17c72
Hamzasehouli <sehoulihamza@yahoo.com>
2022-03-05 18:59:47 +0100

    Changed footer bg color

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,36 +1,36 @@
-<footer id="footer" class="relative z-50 dark:bg-pink-700">
-    <div tabindex="0" aria-label="footer" class="focus:outline-none border-t  border-gray-200 dark:border-gray-700 py-16">
+<footer id="footer" class="relative z-50 bg-black text-white">
+    <div tabindex="0" aria-label="footer" class="focus:outline-none border-t border-gray-200 dark:border-gray-700 py-8">
         <div class="mx-auto container px-4 xl:px-12 2xl:px-4">
             <div class="lg:flex">
                 <div class="w-full lg:w-1/2 mb-16 lg:mb-0 flex">
                     <div class="w-full lg:w-1/2 px-6">
                         <ul>
-                            <li><a class="focus:outline-none focus:underline  text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">Components</a></li>
-                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">Templates</a></li>
-                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">Pricing</a></li>
-                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">FAQ</a></li>
-                            <li class="mt-6"><a href="javascript:void(0)" class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">Documentation</a></li>
+                            <li><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand " href="javascript:void(0)">Components</a></li>
+                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  " href="javascript:void(0)">Templates</a></li>
+                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  " href="javascript:void(0)">Pricing</a></li>
+                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  " href="javascript:void(0)">FAQ</a></li>
+                            <li class="mt-6"><a href="javascript:void(0)" class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  ">Documentation</a></li>
                         </ul>
                     </div>
                     <div class="w-full lg:w-1/2 px-6">
                         <ul>
-                            <li><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">Free components</a></li>
-                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">Blog</a></li>
-                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">Changelog</a></li>
+                            <li><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  " href="javascript:void(0)">Free components</a></li>
+                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  " href="javascript:void(0)">Blog</a></li>
+                            <li class="mt-6"><a class="focus:outline-none focus:underline text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  " href="javascript:void(0)">Changelog</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="w-full lg:w-1/2 flex">
                     <div class="w-full lg:w-1/2 px-6">
                         <ul>
-                            <li><a href="javascript:void(0)" class="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">Privacy policy</a></li>
-                            <li class="mt-6"><a class="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50" href="javascript:void(0)">Terms of service</a></li>
+                            <li><a href="javascript:void(0)" class="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  ">Privacy policy</a></li>
+                            <li class="mt-6"><a class="focus:underline focus:outline-none text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand  " href="javascript:void(0)">Terms of service</a></li>
                         </ul>
                     </div>
                     <div class="w-full lg:w-1/2 px-6 flex flex-col justify-between">
                         <div class="flex items-center mb-6">
                             <a  aria-label="Github" href="javascript:void(0)">
-                                <div class="text-gray-800 dark:text-gray-50 cursor-pointer hover:text-brand dark:hover:text-brand">
+                                <div class="  cursor-pointer hover:text-brand dark:hover:text-brand">
                                    <img class="dark:hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg1.svg" alt="Github">
                                    <img class="dark:block hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg1dark.svg" alt="Github">
                                 </div> </a
@@ -42,12 +42,12 @@
                             </a>
                         </div>
                         {{-- <div class="relative w-36">
-                            <img id="auto" class="dark:hidden absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-device-laptop" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg3.svg" alt="auto">
-                            <img id="auto" class="dark:block hidden absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-device-laptop" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg3dark.svg" alt="auto">
-                            <img  id="light" class="dark:hidden hidden absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-brightness-up" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg4.svg" alt="light">
-                            <img  id="light" class="dark:block hidden hidden absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-brightness-up" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg4dark.svg" alt="light">
-                            <img id="dark" class="dark:hidden hidden absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-moon" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg5.svg" alt="dark">
-                            <img id="dark" class="dark:block hidden hidden absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-moon" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg5dark.svg" alt="dark">
+                            <img id="auto" class="dark:hidden absolute inset-0 m-auto ml-3 text-gray-700  icon icon-tabler icon-tabler-device-laptop" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg3.svg" alt="auto">
+                            <img id="auto" class="dark:block hidden absolute inset-0 m-auto ml-3 text-gray-700  icon icon-tabler icon-tabler-device-laptop" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg3dark.svg" alt="auto">
+                            <img  id="light" class="dark:hidden hidden absolute inset-0 m-auto ml-3 text-gray-700  icon icon-tabler icon-tabler-brightness-up" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg4.svg" alt="light">
+                            <img  id="light" class="dark:block hidden hidden absolute inset-0 m-auto ml-3 text-gray-700  icon icon-tabler icon-tabler-brightness-up" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg4dark.svg" alt="light">
+                            <img id="dark" class="dark:hidden hidden absolute inset-0 m-auto ml-3 text-gray-700  icon icon-tabler icon-tabler-moon" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg5.svg" alt="dark">
+                            <img id="dark" class="dark:block hidden hidden absolute inset-0 m-auto ml-3 text-gray-700  icon icon-tabler icon-tabler-moon" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg5dark.svg" alt="dark">
                             
                         </div> --}}
                     </div>
@@ -55,12 +55,12 @@
             </div>
         </div>
     </div>
-    <div class="py-16 flex flex-col justify-center items-center">
+    <div class="py-5 flex flex-col justify-center items-center">
         {{-- <a class="focus:outline-none" tabindex="0" role="link" aria-label="home link" href="javascript:void(0)"
             >
             <img class="dark:hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg7.svg" alt="tuk logo">
             <img class="dark:block hidden" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/footer_1-svg7dark.svg" alt="tuk logo">
         </a> --}}
-        <p tabindex="0" class="focus:outline-none mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">2022 Caketogo. All Rights Reserved.</p>
+        <p tabindex="0" class="focus:outline-none mt-6 text-xs lg:text-sm leading-none ">2022 Caketogo. All Rights Reserved.</p>
     </div>
 </footer>
